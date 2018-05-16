271fcaa68f66528fc16bd1414d99e6b0fecb15e0
Piotr95T <p.tatar95@gmail.com>
2018-05-16 21:35:29 +0200

    index 1f v1 + js do 1f

---
+++ www/js/Zabytki.js
@@ -0,0 +1,55 @@
+function changeFunc($value) {
+  if($value == "1"){
+    draw1();
+  }else if ($value == "2"){
+    draw2();
+  }else if ($value == "3"){
+    draw3();
+  }else if ($value == "4"){
+    draw4();
+  }else if ($value == "5"){
+    draw5();
+  }else if ($value == "6"){
+    draw6();
+  }else if ($value == "7"){
+    draw7();
+  }else if ($value == "8"){
+    draw8();
+  }else if ($value == "9"){
+    draw9();
+  }else if ($value == "10"){
+    draw10();
+  }else if ($value == "11"){
+    draw11();
+  }else if ($value == "12"){
+    draw12();
+  }else if ($value == "13"){
+    draw13();
+  }else if ($value == "14"){
+    draw14();
+  }else if ($value == "15"){
+    draw15();
+  }else if ($value == "16"){
+    draw16();
+  }else if ($value == "17"){
+    draw17();
+  }else if ($value == "18"){
+    draw18(); 
+  }else if ($value == "19"){
+    draw19();
+  }else if ($value == "20"){
+    draw20(); 
+  }else if ($value == "21"){
+    draw21();
+  }  
+}
+
+function checkInformation($value){
+var button = document.getElementById("building-information-button");
+
+  if($value == "13" || $value == "14" || $value == "15" || $value == "16" ||  $value == "17" ||  $value == "18" ||  $value == "19" ||  $value == "20" ||  $value == "21"){
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
+}
