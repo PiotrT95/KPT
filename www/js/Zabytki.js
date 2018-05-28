9444039795faf56a8e13e6dfb4f81b79f3659a5d
Piotr95T <p.tatar95@gmail.com>
2018-05-28 14:08:49 +0200

    test

--- www/js/Zabytki.js
+++ www/js/Zabytki.js
@@ -27,6 +27,20 @@ function changeFunc($value) {
     draw13();
   }else if ($value == "14"){
     draw14();
+  }else if ($value == "15"){
+    draw15();
+  }else if ($value == "16"){
+    draw16();
+  }else if ($value == "17"){
+    draw17();
+  }else if ($value == "18"){
+    draw18(); 
+  }else if ($value == "19"){
+    draw19();
+  }else if ($value == "20"){
+    draw20(); 
+  }else if ($value == "21"){
+    draw21();
   }  
 }
 
@@ -38,4 +52,268 @@ var button = document.getElementById("building-information-button");
   } else {
     button.disabled = false;
   }
+}
+
+function draw() {
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+    img = new Image;
+img.onload = function() {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(this, 0, 0);
+};
+img.src = "img/mapa.png";
+
+}
+
+function draw1() {
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+    img = new Image;
+img.onload = function() {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(this, 0, 0);
+};
+img.src = "img/wawelloc.jpg";
+}
+function draw2() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 11, 111);
+};
+img.src = "img/biblioteka.png";
+
+}
+
+function draw3() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 11, 242);
+};
+img.src = "img/a.png";
+
+}
+
+function draw4() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 11, 176);
+};
+img.src = "img/b.png";
+
+}
+
+function draw5() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 11, 193);
+};
+img.src = "img/c.png";
+
+}
+
+function draw6() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 10, 275);
+};
+img.src = "img/d.png";
+
+}
+
+function draw7() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 11, 147);
+};
+img.src = "img/e.png";
+
+}
+
+function draw8() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 15, 36);
+};
+img.src = "img/f.png";
+
+}
+
+function draw9() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 14, 343);
+};
+img.src = "img/g.png";
+
+}
+
+function draw10() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 131, 126);
+};
+img.src = "img/ustronie.png";
+
+}
+
+function draw11() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 110, 0);
+};
+img.src = "img/sportowy.png";
+
+}
+
+function draw12() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 46, 305);
+};
+img.src = "img/ksiezowka.png";
+
+}
+
+function draw13() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 85, 343);
+};
+img.src = "img/ogrodnika.png";
+
+}
+
+function draw14() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 208, 344);
+};
+img.src = "img/strozowka.png";
+
+}
+
+function draw15() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 26, 154);
+};
+img.src = "img/zauek.png";
+
+}
+
+function draw16() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 53, 333);
+};
+img.src = "img/grota.png";
+
+}
+
+function draw17() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 64, 46);
+};
+img.src = "img/bulka.png";
+
+}
+
+function draw18() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 80, 255);
+};
+img.src = "img/kiosk.png";
+
+}
+
+function draw19() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 79, 53);
+};
+img.src = "img/bankomat.png";
+
+}
+
+function draw20() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 127, 183);
+};
+img.src = "img/boisko.png";
+
+}
+
+function draw21() {
+var ctx = document.querySelector("canvas").getContext("2d"),
+    img = new Image;
+    draw();
+img.onload = function() {
+  
+  ctx.drawImage(this, 171, 183);
+};
+img.src = "img/kort.png";
+
 }
--- www/js/database.js
+++ www/js/database.js
@@ -1,8 +1,54 @@
  //document.addEventListener("deviceready", onDeviceReady, false);
-    var db = window.openDatabase("Test_Database9", "1.0", "Test DB", 200000);
+    var db = window.openDatabase("Test_Database12", "4.0", "Test DB3", 200000);
     var globalQrID;
     var globalDate;
+  
 
+        //function will be called when device ready
+    function onContactsActivityReady(){
+        db.transaction(populateContactsDB, errorContactsCB, successContactsCB);
+    }
+ 
+    //create table and insert some record
+    function populateContactsDB(tx1) {
+        tx1.executeSql('CREATE TABLE IF NOT EXISTS Contact (id INTEGER PRIMARY KEY AUTOINCREMENT, ContactName TEXT NOT NULL, ContactNumber TEXT NOT NULL, ContactMail TEXT NOT NULL, UNIQUE(ContactName))');
+        
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Parlament Studencki Uniwersytetu Ekonomicznego w Krakowie", "12 2935585", "biuro@psuek.pl")'); 
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Biblioteka Główna Uniwersytetu Ekonomicznego w Krakowie", "12 2935009", "bguek@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Kierownik Dziekanatu Zarządzania", "12 2935724", "bienk@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Kierownik Dziekanatu Towaroznawstwa", "12 2935608", "nosalh@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Kierownik Dziekanatu Gospodarki i Administracji Publicznej", "12 2935920", "smolinsd@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Kierownik Dzienakatu Finansów i Prawa ", "12 2935843", "okreglig@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Dyrektor Kancelarii Rektora", "12 2935608", "kancelaria@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Kierownik Działu Spraw Bytowych", "12 2935130", "krupat@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Biuro ds. Osób Niepełnosprawnych", "12 2935120", "bon@uek.krakow.pl")');
+        tx1.executeSql('INSERT INTO Contact(ContactName,ContactNumber, ContactMail) VALUES ("Sekretariat Studium WF", "12 2935070", "swfis@uek.krakow.pl ")');
+        }
+ 
+    function errorContactsCB(err) {
+        db.transaction(queryContactsDB,errorContactsCB);
+    }
+ 
+    function successContactsCB() {
+        db.transaction(queryContactsDB,errorContactsCB);
+    }
+ 
+    function queryContactsDB(tx1){
+        tx1.executeSql('SELECT * FROM Contact',[],queryContactsSuccess,errorContactsCB); 
+    }
+ 
+    function queryContactsSuccess(tx1,result1){
+
+        $('#contacts').empty();
+        $.each(result1.rows,function(index){
+            var row = result1.rows.item(index);
+
+            $('#contacts').append('<li class="ui-li-static ui-body-inherit"><h3 class="ui-li-heading">'+row['ContactName']+'</h3><p class="ui-li-desc">Telefon: <a href="tel:'+row['ContactNumber']+'">'+row['ContactNumber']+'</a></p><p class="ui-li-desc">E-Mail: <a href="mailto:'+row['ContactMail']+'">'+row['ContactMail']+'</a></p></li>');
+        });
+ 
+        $('#contacts').listview();
+                        
+    }
 
     function onBuildingActivityReady(){
 
@@ -13,21 +59,19 @@
  
     //create table and insert some record
     function populateBuildingDB(tx2) {
-        tx2.executeSql('CREATE TABLE IF NOT EXISTS Building (id INTEGER PRIMARY KEY AUTOINCREMENT, BuildingName TEXT NOT NULL, BuildingPhoto TEXT NOT NULL, BuildingInformation TEXT NOT NULL, UNIQUE(BuildingName))');   
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Wawel", "<img id=theImg src=img/wawel.jpg />", "Test1")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Smocza Jama", "<img id=theImg src=img/smoczajama.jpg />", "Test2")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Kościół św. Piotra i Pawła", "<img id=theImg src=img/kosciolpiotrpawel.jpg />",  "Test3")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Kościół Mariacki", "<img id=theImg src=img/kosciolmariacki.jpg />",  "Test4")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Rynek Główny", "<img id=theImg src=img/rynekglowny.jpg />",  "Test5")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Sukiennice", "<img id=theImg src=img/sukienice.jpg />",  "Test6")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Wieża Ratuszowa", "<img id=theImg src=img/wiezaratuszowa.jpg />",  "Test7")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Brama Floriańska", "<img id=theImg src=img/bramaflorianska.jpg />",  "Test8")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Planty", "<img id=theImg src=img/planty.jpg />", "Test9")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Barbakan Krakowski", "<img id=theImg src=img/barbakankrakowski.jpg />", "Test10")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Teatr im.Juliusza Słowackiego", "<img id=theImg src=img/teatrslowackiego.jpg />", "Test11")'); 
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Plac Szczepański", "<img id=theImg src=img/palacszczepanski.jpg />", "Test12")');
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Okno Papieskie", "<img id=theImg src=img/oknopapieskie.jpg />",  "Test13")');
-        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingInformation) VALUES ("Uniwersytet Jagieloński", "<img id=theImg src=img/uj.jpg />",  "Test14")'); 
+        tx2.executeSql('CREATE TABLE IF NOT EXISTS Building (id INTEGER PRIMARY KEY AUTOINCREMENT, BuildingName TEXT NOT NULL, BuildingPhoto TEXT NOT NULL, BuildingLocalization TEXT NOT NULL, BuildingUsefulPlaces TEXT NOT NULL, BuildingRooms TEXT NOT NULL, UNIQUE(BuildingName))');   
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Wawel", "<img id=theImg src=img/wawel.jpg />", "<img id=theImg src=img/wawelloc.png />", "Dziekanaty:<br> Godziny otwarcia: <br>poniedziałek, wtorek, czwartek w godzinach: 11.00-14.00<br>piątek w godzinach: 11.00-14.00 i 15:00-17:00<br>środa - dziekanat nieczynny <br> sobota  w godzinach: 8.00-12.00 ", "-Piwnica: 1 (sala obron), 2 (sala obron), 3, 4, 5, 6 <br>-1 piętro: 7, 8, Stara Aula")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Smocza Jama", "<img id=theImg src=img/smoczajama.jpg />", "<img id=theImg src=img/bibloc.png />", "Wypożyczalnia ogólna - 1 piętro (p.102):<br> Godziny otwarcia: <br>Pn. 10:00 - 18:00<br>Wt. - Pt. 9:00 - 18:00<br>Sob. 9:00 - 15:00<br>Nd. 9:00 - 14:00", "-4 piętro: 440, 441, 442, 443")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Kościół św. Apostołów Piotra i Pawła", "<img id=theImg src=img/kosciolpiotrpawel.jpg />", "<img id=theImg src=img/aloc.png />", "Sale pracy samodzielnej - parter (07, 09, 013, 014)<br>Godziny pracy samodzielnej pod adresem <a onclick=loadPage(http://uek.krakow.pl/pl/uczelnia/administracja-i-inne-jednostki/pion-kanclerza/jednostki-bezposrednio-podlegle-kanclerzowi/centrum-informatyki/laboratoria/praca-indywidualna.html);>http://uek.krakow.pl/pl/uczelnia/administracja-i-inne-jednostki/pion-kanclerza/jednostki-bezposrednio-podlegle-kanclerzowi/centrum-informatyki/laboratoria/praca-indywidualna.html</a>", "-Parter: 07, 09, 013, 014 <br>-1 piętro: 115, 116, 117, 118, 121, 122<br>-2 piętro: 201 <br>-3 piętro: 301a, 301b <br>-4 piętro: 410, 413")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Rynek Główny", "<img id=theImg src=img/rynekglowny.jpg />", "<img id=theImg src=img/bloc.png />", "-460(sekretariat katedry przedsiębiorczości i innowacji)<br>-469(sekretariat katedry nauk politycznych)", "-Parter: 052, 053, 054, 055, 056, 057, 058, 059 <br> 1 piętro: 151, 152, 153 <br>-2 piętro: 251 <br>-3 piętro: 351 <br>-4 piętro: 451")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Sukiennice", "<img id=theImg src=img/sukienice.jpg />", "<img id=theImg src=img/cloc.png />", "-Siłownia (piwnica)<br>-Chór Dominanta (p.01a - parter)", "-Parter: A, B, C, D <br>-1 piętro: Nowa Aula, E, F")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Wieża Ratuszowa", "<img id=theImg src=img/wiezaratuszowa.jpg />", "<img id=theImg src=img/dloc.png />", "-Gabinet psychologa (p.18)<br>-Zrzeszenie studentów niepełnosprawnych (p.19)<br>-Biuro ds. osób niepełnosprawnych (p.20)", "-Parter: 3, 4, 5, 6, Prac. Mikrob. 1, Prac. Mikrob. 2")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Kościół Mariacki", "<img id=theImg src=img/kosciolmariacki.jpg />", "<img id=theImg src=img/eloc.png />", "-Klub Studencki ZaUEK - wejście od pola<br>Więcej informacji pod adresem <a onclick=loadPage(https://www.facebook.com/zauek/);>https://www.facebook.com/zauek/</a>", "-Parter: G, H <br>-1 piętro: J")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Brama Floriańska", "<img id=theImg src=img/bramaflorianska.jpg />", "<img id=theImg src=img/floc.png />", "-Restauracja Bułka z makiem - parter<br>-Bankomat Euronet - parter", "-Parter: 011(SJO), 012(SJO) <br>-1 piętro: 102, 104, 106, 107, 109, 111<br>-2 piętro: 210(SJO)<br>-3 piętro: 301(SJO), 302(SJO), 303(SJO), 309(SJO), 310(SJO), 311(SJO), 312(SJO), 313(SJO), 314(SJO)<br>-4 piętro: 413, 414, 415, 416<br>-5 piętro: 513, 514, 515, 516<br>-6 piętro: 613, 614, 615, 616<br>-7 piętro: 713, 715, 717")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Planty", "<img id=theImg src=img/planty.jpg />", "<img id=theImg src=img/gloc.png />", "---", "-Piwnica: 1, 2, 3 <br>-Parter: 4, 5, 6, 7 <br>-1 piętro: 8, 9, 10, 11<br>-2 piętro: 12, 14, 15")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Teatr im. Juliusza Słowackiego", "<img id=theImg src=img/teatrslowackiego.jpg />", "<img id=theImg src=img/uloc.png />", "-Stołówka UEK<br>Godziny otwarcia:<br>poniedziałek–piątek: 10:00–16:30<br> do godziny 11:30 dania barowe, po godzinie 11:30 obiady", "-1 piętro: K, L, M, N <br>-2 piętro: O, P, S <br>-3 piętro: T, Lab. Fiz. 1, Lab. Fiz. 2 <br>-4 piętro: Lab. Prz. 1, Lab. Opakowań 1")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Okno Papieskie", "<img id=theImg src=img/oknopapieskie.jpg />", "<img id=theImg src=img/sloc.png />", "Restauracja Dobre smaki<br>Godziny otwarcia: <br>poniedziałek–sobota: 8:00–19:00<br> niedziela: 8:00–16:00", "-Piwnica: Szatnie, Wejście na halę sportową <br>-1 piętro: 1, 2, 3, 4, 5 <br>-2 piętro: 6, 7, 8, 9")'); 
+        tx2.executeSql('INSERT INTO Building(BuildingName,BuildingPhoto,BuildingLocalization,BuildingUsefulPlaces,BuildingRooms) VALUES ("Universitet Jagieloński", "<img id=theImg src=img/uj.jpg />", "<img id=theImg src=img/kloc.png />", "Klub studencki Grota 2 <br>Godziny otwarcia: <br>poniedziałek–piątek:  9:00–22:00<br> sobota: 9:00–17:00<br> niedziela: 9:00–15:00", "-Parter: Sala seminaryjna")'); 
         }
  
     function errorBuildingCB(err) {
@@ -49,15 +93,18 @@
         $('#buildingName').empty();
         $('#buildingDesc').empty();
         $('#buildingPhoto').empty();
-        $('#buildingInformation').empty();
+        $('#buildingLocalization').empty();
+        $('#buildingRooms').empty();
+        $('#buildingUsefulPlaces').empty();
         $.each(result2.rows,function(index){
             var row = result2.rows.item(index);
 
             $('#buildingName').append(row['BuildingName']);
             $('#buildingDesc').append(row['BuildingDesc']);
             $('#buildingPhoto').prepend(row['BuildingPhoto'])
-            $('#buildingInformation').prepend(row['BuildingInformation'])
-            
+            $('#buildingLocalization').prepend(row['BuildingLocalization'])
+            $('#buildingRooms').append(row['BuildingRooms']);
+            $('#buildingUsefulPlaces').append(row['BuildingUsefulPlaces']);
         });
                         
     }
@@ -86,16 +133,18 @@
         $('#buildingName').empty();
         $('#buildingDesc').empty();
         $('#buildingPhoto').empty();
-        $('#buildingInformation').empty();
-
+        $('#buildingLocalization').empty();
+        $('#buildingRooms').empty();
+        $('#buildingUsefulPlaces').empty();
         $.each(result2.rows,function(index){
             var row = result2.rows.item(index);
 
             $('#buildingName').append(row['BuildingName']);
             $('#buildingDesc').append(row['BuildingDesc']);
             $('#buildingPhoto').prepend(row['BuildingPhoto'])
-            $('#buildingInformation').prepend(row['BuildingInformation'])
-
+            $('#buildingLocalization').prepend(row['BuildingLocalization'])
+            $('#buildingRooms').append(row['BuildingRooms']);
+            $('#buildingUsefulPlaces').append(row['BuildingUsefulPlaces']);
         });
                         
     }
